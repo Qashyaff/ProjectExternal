eff94d9d6c12d8385b1ff1eb11e08d1146cf959f
luqman <alluqmanqashyaffazmi@gmail.com>
2022-10-11 15:25:33 +0800

    system updated

---
+++ ResumeProject/resources/views/Userportfolio/contactnumberpage.blade.php
@@ -0,0 +1,8 @@
+@extends ('layout.mainlayout')
+@section('content')
+
+<h3> this is my contact number  page</h3>
+
+
+
+@endsection
---
+++ ResumeProject/resources/views/Userportfolio/instagrampage.blade.php
@@ -0,0 +1,8 @@
+@extends ('layout.mainlayout')
+@section('content')
+
+<h3> this is my instagram page</h3>
+
+
+
+@endsection
