586926da9fd1735e033ec8710da8b18e6ace1428
luqman <alluqmanqashyaffazmi@gmail.com>
2022-11-16 10:41:31 +0800

    updatedsystem

--- ResumeProject/resources/views/myPDF.blade.php
+++ ResumeProject/resources/views/myPDF.blade.php
@@ -45,7 +45,7 @@ nav {
   float: left;
   width: 30%;
   height: 300px; /* only for demonstration, should be removed */
-  background: black;
+  background: gray;
   padding: 20px;
 }
 
@@ -64,7 +64,7 @@ article {
 }
 
 
-article.2 {
+.article2 {
   float: right;
   padding: 20px;
   width: 70%;
@@ -98,67 +98,99 @@ footer {
 <body>
 <form method="POST" action="{{ route('generatePDF') }}">
     <div class="a">
-    <img src="https://cdn1.vectorstock.com/i/1000x1000/23/70/man-avatar-icon-flat-vector-19152370.jpg" alt="" width="200" height="150">
+    <img src="https://media-exp1.licdn.com/dms/image/C4D03AQEC-0BMwWR-vw/profile-displayphoto-shrink_800_800/0/1653916143708?e=2147483647&v=beta&t=GaQ8FVQaZix3KD3RhJ6_XUO8qTjaBmdlb-YJD8dx_fc.jpg" alt="" width="200" height="150">
 </div>
 
     
     
     <h2> My Resume</h2>
-<p>In this example, we have created a header, two columns/boxes and a footer. On smaller screens, the columns will stack on top of each other.</p>
-<p>Resize the browser window to see the responsive effect (you will learn more about this in our next chapter - HTML Responsive.)</p>
-
 <header>
   <h2>{{$Name}}</h2>
+  <?php echo date('d.m.Y');?>
 </header>
 
-<section>
-  <nav>
-    <ul>
-      <li><a href="#">About Me</a></li>
-      <li><a href="#">My working Experience</a></li>
-      <li><a href="#">Test1</a></li>
-      <li><a href="#">Test1</a></li>
-      <li><a href="#">Test1</a></li>
-      <li><a href="#">Test1</a></li>
-    </ul>
-  </nav>
+
+
   
   <article>
-    <h1>London</h1>
-    <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
-    <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+    <h1>Information Technology</h1>
+    <p>Upcoming University Tenaga Nasional 
+graduate offering a strong foundation 
+and bachelor’s in software engineering 
+and programming principles across 
+multiple platforms. Experienced in 
+object-oriented programming, 
+developing, testing and debugging 
+code, designing interfaces and 
+administering systems and networks. 
+Quick learn ad master new 
+technologies, successful working in 
+both team and self-directed settings.</p>
   </article>
+</section>
   
-  
-  <article.2>
-    <h1>Tester1</h1>
-    <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
-    <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
-  </article.2>
+ 
  
  
  <article>
-    <h1>Tester2</h1>
-    <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
-    <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+    <h1>Skills</h1>
+    <ul>
+  <li>Microsoft Office</li>
+    <li>Power point</li>
+    <li>Excel</li>
+    <li>Visio</li>
+   <li>Programming</li>
+   <li>HTML</li>
+   <li> CSS</li>
+   <li>Javascript</li>
+   <li>PHP</li>
+   <li>WordPress</li>
+  
+  
+  </ul>
   </article>
  
   
- <article.2>
-    <h1>Tester3</h1>
+ <article2>
+    <h1>Working Experience</h1>
     <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
     <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
-  </article.2>
+  </article2>
+</section>
+    
+ <article2>
+    <h1>Education</h1>
+    <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
+    <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+  </article2>
+  
+      
+ <article2>
+    <h1>Working experience</h1>
+    <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
+    <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+  </article2>
+  
+  
   
   </section>
   
 
 
-<footer>
+
   <p>This is my social media</p> 
+  <ul>
+  <li>Facebook</li>
+   <li>Linked In</li>
+    <li>Gmail</li> 
+     <li>Social media1</li>
+      <li>Social media2</li>
+       <li>Social media3</li>
+  </ul>
   <p>
 </p>
-
+<footer>
+<p>This is my social media</p> 
 </footer>
 
 
